23759be7286870def6b2cb72a1477c27e31f4e5f
Hüseyin Emecan <emecanh@gmail.com>
2018-11-19 23:11:07 +0300

    W4
    Find a name example

---
+++ 02_İsimBulmayaCalis/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_İsimBulmayaCalis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // KULLANICIDAN "AHMET" GİRİLENE KADAR İSİM İSTEYEN PROGRAM
+            // ahmet girildiğinde kaç denemede bildiğini sorun
+
+            byte sayac = 0;
+
+            const string sabitisim = "ahmet";
+            string girilenisim;
+            isimal:
+            Console.Write("İsim Girin : ");
+            girilenisim = Console.ReadLine().Trim().ToLower();
+            sayac++;
+            // sayac = sayac + 1;  
+            // byte değişkenler kucuk değerlerle çalıştıgı ıcın toplama , çartpma gibi işlemlerde hata verebilir.
+            if (girilenisim == sabitisim)
+            {
+                Console.WriteLine($"DOĞRU BİLDİN. Deneme Sayısı {sayac}");
+            }
+            else
+            {
+                Console.WriteLine("YANLIŞ.. ");goto isimal;
+            }
+
+
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
